711d23be08025bf0e2527abb393d2e7c18db8b19
Brett Cloward <brettcloward@mail.weber.edu>
2018-11-16 02:14:33 +0000

    3rd Commit

--- task1/ManchesterUnited_Module2.py
+++ task1/ManchesterUnited_Module2.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 """
+
 """
 from __future__ import print_function
-import ManchesterUnited_Module1 #from ManchesterUnited_Module1 import ___#
+from ManchesterUnited_Module1 import module_one
+from urllib.request import urlopen
 
+
+def module_two():
+    test_file = []
+    with urlopen('www.icarus.cs.weber.edu/~hvalle/cs3030/data/minivanTest.csv') as the_file:
+        for word in the_file:
+            test_file.append(word.decode("utf-8"))
+    print(test_file)
 
 def main():
     """
     Test your module
     """
-
-    pass
+    module_two()
 
 
 if __name__ == "__main__":
